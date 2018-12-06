12d86e75a8bb1a347bcbc2a30a8d3a45123078e8
VentGrey <omarpurataf@gmail.com>
2018-12-05 22:20:19 -0600

    Se agregó la comprobación a el ingreso de clientes.

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ typedef struct {
         char apaterno[15];
         char amaterno[15];
         unsigned int edad;
-}cliente;
+}Cliente;
 
 typedef struct {
         char nombre[20];
@@ -60,6 +60,66 @@ void menu() {
 
                         case 4: break;
                 }
-        } while (opcion != 5);
+        } while (opcion != 4);
 
+}
+
+void alta_clientes() {
+        FILE *fp;
+
+        Cliente client;
+        fp = fp("Clientes.res");
+
+        // Datos del cliente
+        printf("Ingrese el ID del cliente: ");
+        scanf("%i",&client.ID);
+
+        if(client.ID == 0) {
+                client.ID = NULL;
+                printf("No se permiten campos nulos");
+                alta_clientes();
+        }
+
+        printf("\nIngrese el correo del cliente: ");
+        scanf("%s",&client.correo);
+        if(strlen(client.correo) == 0) {
+                client.correo = NULL;
+                printf("No se permiten campos nulos");
+                alta_clientes();
+        }
+
+        printf("\nIngrese el nombre de usuario: ");
+        scanf("%s",&client.nombre);
+        if(strlen(client.nombre) == 0) {
+                client.nombre = NULL;
+                printf("No se permiten campos nulos");
+                alta_clientes();
+        }
+
+        printf("\nIngrese el apellido paterno del usuario: ");
+        scanf("%s",&client.apaterno);
+        if(strlen(client.apaterno) == 0) {
+                client.apaterno = NULL;
+                printf("No se permiten campos nulos");
+                alta_clientes();
+        }
+
+        printf("\nIngrese el apellido materno del usuario");
+        scanf("%s",&client.amaterno);
+        if(strlen(client.amaterno) == 0) {
+                client.amaterno = NULL;
+                printf("No se permiten campos nulos");
+                alta_clientes();
+        }
+
+        printf("\nIngrese la edad del usuario: ");
+        scanf("%s",&client.edad);
+        if(client.edad== 0) {
+                client.edad = NULL;
+                printf("No se permiten campos nulos");
+                alta_clientes();
+        }
+
+
+        //Datos de las películas
 }
