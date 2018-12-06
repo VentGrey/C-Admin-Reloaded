8b3d5bdb3ca074be1d94315f9159d97b48641054
VentGrey <omarpurataf@gmail.com>
2018-12-05 22:29:48 -0600

    Se agregaron validacones para evitar el exceso de carácteres

--- main.c
+++ main.c
@@ -80,38 +80,51 @@ void alta_clientes() {
                 alta_clientes();
         }
 
+        // Ingreso de correo
         printf("\nIngrese el correo del cliente: ");
         scanf("%s",&client.correo);
         if(strlen(client.correo) == 0) {
                 client.correo = NULL;
                 printf("No se permiten campos nulos");
                 alta_clientes();
+        } else if(strlen(client.correo) > 50) {
+                printf("Se excedió el número de caracteres aceptados");
         }
 
+        // Ingreso de Username
         printf("\nIngrese el nombre de usuario: ");
         scanf("%s",&client.nombre);
         if(strlen(client.nombre) == 0) {
                 client.nombre = NULL;
                 printf("No se permiten campos nulos");
                 alta_clientes();
+        } else if(strlen(client.correo) > 15) {
+                printf("Se excedió el número de caracteres aceptados");
         }
 
+        // Ingreso de Apellido Paterno
         printf("\nIngrese el apellido paterno del usuario: ");
         scanf("%s",&client.apaterno);
         if(strlen(client.apaterno) == 0) {
                 client.apaterno = NULL;
                 printf("No se permiten campos nulos");
                 alta_clientes();
+        }else if(strlen(client.correo) > 15) {
+                printf("Se excedió el número de caracteres aceptados");
         }
 
+        // Ingreso de apellido materno
         printf("\nIngrese el apellido materno del usuario");
         scanf("%s",&client.amaterno);
         if(strlen(client.amaterno) == 0) {
                 client.amaterno = NULL;
                 printf("No se permiten campos nulos");
                 alta_clientes();
+        }else if(strlen(client.correo) > 15) {
+                printf("Se excedió el número de caracteres aceptados");
         }
 
+        // Ingreso de la edad del usuario
         printf("\nIngrese la edad del usuario: ");
         scanf("%s",&client.edad);
         if(client.edad== 0) {
