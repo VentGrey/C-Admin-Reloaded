9ead39a482dc21c6bdb45c8bb2d3efeaf4b2d1bf
VentGrey <omarpurataf@gmail.com>
2018-12-09 19:57:33 -0600

    Menú colorizado

--- main.c
+++ main.c
@@ -61,17 +61,17 @@ void menu() {
 
         do {
                 printf(ANSI_COLOR_RED "---------- FLIXNET ----------\n" ANSI_COLOR_RESET);
-                printf("-----Clientes------------------\n");
+                printf(ANSI_COLOR_BLUE "-----Clientes------------------\n" ANSI_COLOR_RESET);
                 printf("1- Dar de alta un cliente\n");
                 printf("2- Dar de baja un cliente\n");
                 printf("3- Modificar un cliente\n");
                 printf("4- Listar los clientes\n");
-                printf("-----Películas-------------------");
+                printf(ANSI_COLOR_GREEN "-----Películas-------------------\n" ANSI_COLOR_RESET);
                 printf("5- Dar de alta una película\n");
                 printf("6- Dar de baja una película\n");
                 printf("7- Modificar una película\n");
                 printf("8- Listar las películas\n");
-                printf("9- Salir\n");
+                printf(ANSI_COLOR_RED "9- Salir\n" ANSI_COLOR_RESET);
                 scanf("%d",&op);
 
                 switch(opcion) {
